24146aa80e115b6fa24c133da4d095844a1379b3
Emma Lamont <ejwl_92@hotmail.com>
2024-03-07 12:59:45 +0000

    Create get_exam_data function to get data string from user

--- run.py
+++ run.py
@@ -14,8 +14,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('teacher-tap')
 
-datasheet = SHEET.worksheet('datasheet')
+def get_exam_data():
+    """
+    Get exam result data from user
+    """
+    print("Please enter exam result and predicted grade for each student.")
+    print("Data should be in the format 'Student Name, Predicted Grade, Exam Mark'.")
+    print("Example: Joe Bloggs, 7, 54\n")
 
-data = datasheet.get_all_values()
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
 
-print(data)
+get_exam_data()
