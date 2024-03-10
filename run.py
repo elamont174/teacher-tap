5509b84d540a8143bd8f83fa70707fc9eba3b595
Emma Lamont <ejwl_92@hotmail.com>
2024-03-10 14:00:06 +0000

    Add user request loop to repeat request for data until valid

--- run.py
+++ run.py
@@ -18,24 +18,37 @@ def get_exam_data():
     """
     Get exam result data from user
     """
-    print("Please enter exam result and predicted grade for each student.")
-    print("Data should be in the format 'Student Name, Predicted Grade, Exam Mark'.")
-    print("Example: Joe Bloggs, 7, 54\n")
+    while True:
+        print("Please enter exam result and predicted grade for each student.")
+        print("Data should be in the format 'Student Name, Predicted Grade, Exam Mark'.")
+        print("Example: Joe Bloggs, 7, 54\n")
 
-    data_str = input("Enter your data here: ")
-    
-    exam_data = data_str.split(",")
-    validate_data(exam_data)
+        data_str = input("Enter your data here: ")
+        
+        exam_data = data_str.split(",")
+        validate_data(exam_data)
+
+        if validate_data(exam_data):
+            print("Data is valid")
+            break 
+
+    return exam_data
+
+data = get_exam_data()
 
 def validate_data(values):
     """
     Ensures 'Student Name' has first and surname; Predicted Grade is between 1-9 as an integer; Exam mark is between 0-100 as an integer. Ensures three values added. 
     Raises error if does not fit this format.
     """
+    # How to validate a string and 2 integers in one list? 
     try:
         if len(values) != 3:
             raise ValueError(f"3 values required, you provided {len(values)}")
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")   
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True   
 
 get_exam_data()
